4b148d0dbad872cb85f32126989018219422904f
contimatteo <contimatteo1505@gmail.com>
2018-03-28 18:06:59 +0200

    Updates

--- public/app.js
+++ public/app.js
@@ -1,7 +1,1 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hi everybody!');
-});
-server.listen(8080);
+console.log('hello world');
