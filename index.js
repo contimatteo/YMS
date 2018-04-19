5a69d26593d3c5b9f527325037cff8424eb41e41
contimatteo <contimatteo1505@gmail.com>
2018-04-19 17:03:27 +0200

    Updating configuration for running node on local machine

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ express()
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Ciao Enri come stai?!')
 })
 
 app.listen(app.get('port'), function() {
