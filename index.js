27bf21212412743a74837d64907d94ea402bdbb0
contimatteo <contimatteo1505@gmail.com>
2018-07-22 20:57:20 +0200

    Creating project structure

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-/*const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  //.get('/', (req, res) => res.render('pages/index'))
-  .get('/', (req, res) => res.redirect('app.js'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-*/
-
-var express = require('express')
-var app = express()
-
-app.set('port', (process.env.PORT || 8000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 9000));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  //response.send('Quindi funziona')
-  return response.redirect('/controller.js');
-})
+  //response.send('Progetto TW');
+  response.render('pages/index')
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
