5de825bec13cb098ca01d575f53c24d8872e4327
contimatteo <contimatteo1505@gmail.com>
2018-08-20 16:50:47 +0200

    Updating index

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ require('dotenv').config({
 var bodyParser = require('body-parser');
 ////////////////////////////////////////////////////////////////////////////////
 //                            IMPORT SCHEMA
-// const DBResponse = require('./libs/schema/DBResponse.js');
-// const ApiResponse = require('./libs/schema/ApiResponse.js');
+// ...
 ////////////////////////////////////////////////////////////////////////////////
 //                        INSTANCE GLOBAL OBJECT
 var app = express();
@@ -41,7 +40,7 @@ app.use(express.static(__dirname + '/public'));
 ////////////////////////////////////////////////////////////////////////////////
 // enable lister
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  //console.log("Node app is running at localhost:" + app.get('port'));
   console.log("******************** YMS - Youtube Music Spider [1.0.0] ********************");
   // console.log(process.cwd());
 });
