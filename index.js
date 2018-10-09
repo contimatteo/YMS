5459caede8e2c1677221ce255a830f052c5250b8
contimatteo <contimatteo1505@gmail.com>
2018-10-09 12:34:56 +0200

    add current working index on branch alpha

--- index.js
+++ index.js
@@ -4,27 +4,32 @@ require('dotenv').config({
   path: __dirname + '/.env'
 });
 var bodyParser = require('body-parser');
-
+var passport    =   require('passport');
+var session     =   require('express-session');
+////////////////////////////////////////////////////////////////////////////////
+// INSTANCE GLOBAL OBJECT
 var app = express();
+// app.use(cors({ origin: 'http://italiancoders.it'}));   // project url
 app.use(cors());
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
 }));
-
-app.use(function(req, res, next) {
-  console.log("MIDDLEWARE 1 : controllo di sicurezza passatto correttamente");
-  next();
-});
-
+////////////////////////////////////////////////////////////////////////////////
+// PASSPORT
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+require('./Libraries/Passport.js')(passport);
+////////////////////////////////////////////////////////////////////////////////
+// set enviroment configuration
 app.set('port', (8000 || process.env.PORT || 9000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/static'));
+app.set('views', __dirname + '/webapp/views/');
 app.set('view engine', 'ejs');
-
+////////////////////////////////////////////////////////////////////////////////
 var TestController = require('./Controllers/TestController.js');
 var AjaxRequest = require('./Libraries/AjaxRequest.js');
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 var testView = new TestController();
 var ajaxRequest = new AjaxRequest();
 
@@ -53,4 +58,15 @@ app.get('/sparql', function(request, response) {
   testView.sparql(response);
 });
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'));
+////////////////////////////////////////////////////////////////////////////////
+// PASSPORT
+require('./Libraries/Passport.js')(passport);
+////////////////////////////////////////////////////////////////////////////////
+// LISTENER
+app.listen(app.get('port'), function() {
+  console.log("••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••");
+  console.log("••••••••••••••••• YOUTUBE MUSIC SPIDER ["+app.get('port')+"] •••••••••••••••••");
+  console.log("••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••");
+});
+
+////////////////////////////////////////////////////////////////////////////////
