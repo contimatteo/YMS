fcaeb88b28612dc94dc829e50d50fa98908d76c1
contimatteo <contimatteo1505@gmail.com>
2018-10-10 09:25:35 +0200

    Updating branch production with working index on alpha branch

--- index.js
+++ index.js
@@ -1,71 +1,79 @@
-var express = require('express');
-var cors = require('cors');
+
+// IMPORT MODULE
+var express     =   require('express');
+var ApiRoutes   =   require('./Routes/Api');
+var WebRoutes   =   require('./Routes/Web');
+var AuthRoutes  =   require('./Routes/Auth');
+var cors        =   require('cors');
 require('dotenv').config({
   path: __dirname + '/.env'
 });
-var bodyParser = require('body-parser');
+var bodyParser  =   require('body-parser');
 var passport    =   require('passport');
 var session     =   require('express-session');
+
 ////////////////////////////////////////////////////////////////////////////////
+
+// IMPORT SCHEMA
+// ...
+
+////////////////////////////////////////////////////////////////////////////////
+
 // INSTANCE GLOBAL OBJECT
 var app = express();
 // app.use(cors({ origin: 'http://italiancoders.it'}));   // project url
 app.use(cors());
-var path = require('path');
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
 }));
+
 ////////////////////////////////////////////////////////////////////////////////
+
 // PASSPORT
 app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 require('./Libraries/Passport.js')(passport);
+
 ////////////////////////////////////////////////////////////////////////////////
+
+// MIDDLEWARE 1
+app.use(function(req, res, next) {
+  // console.log("MIDDLEWARE 1 : controllo di sicurezza passatto correttamente");
+  next();
+});
+
+////////////////////////////////////////////////////////////////////////////////
+
+// MIDDLEWARE 2
+app.use(function(req, res, next) {
+  // console.log("MIDDLEWARE 2 : controllo di sicurezza passatto correttamente");
+  next();
+});
+
+////////////////////////////////////////////////////////////////////////////////
+
 // set enviroment configuration
 app.set('port', (8000 || process.env.PORT || 9000));
 app.use(express.static(__dirname + '/static'));
- app.set('views', '/webapp/views');
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-////////////////////////////////////////////////////////////////////////////////
-var TestController = require('./Controllers/TestController.js');
-var AjaxRequest = require('./Libraries/AjaxRequest.js');
-var testView = new TestController();
-var ajaxRequest = new AjaxRequest();
-
-app.get('/', function(request, response) {
-  response.render('dashboard');
-});
-// route for testing db
-app.get('/db', function(request, response) {
-  testView.visualizzoDatiDiProva(response);
-});
-// api testing route
-app.get('/api', function(request, response) {
-  ajaxRequest.jsonRequest("https://reqres.in/api/users", "GET", {}, function(result) {
-    response.send(result.data);
-  });
-});
-// api testing route
-app.get('/youtube', function(request, response) {
-  testView.ricercaVideo(response, "Ninja", 10);
-});
-// api testing route
-app.get('/youtube/:id', function(request, response) {
-  var id = request.params.id;
-  testView.visualizzoVideo(response, id);
-});
-// SPARQL testing route
-app.get('/sparql', function(request, response) {
-  testView.sparql(response);
-});
 
 ////////////////////////////////////////////////////////////////////////////////
+
+// ROUTES
+// ApiRoutes(app, passport);
+WebRoutes(app, passport);
+AuthRoutes(app, passport);
+
+////////////////////////////////////////////////////////////////////////////////
+
 // PASSPORT
 require('./Libraries/Passport.js')(passport);
+
 ////////////////////////////////////////////////////////////////////////////////
+
 // LISTENER
 app.listen(app.get('port'), function() {
   console.log("••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••");
