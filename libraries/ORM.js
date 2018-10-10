a5f7f3ead9f2f47dd9a43d16c7e954b1e365b647
contimatteo <contimatteo1505@gmail.com>
2018-10-10 12:12:02 +0200

    remove wrong console log

--- libraries/ORM.js
+++ libraries/ORM.js
@@ -5,7 +5,6 @@ const Sequelize = require('sequelize');
 module.exports = class myORM {
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   constructor() {
-    console.log(config.development.host);
     this.sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
       host: config.development.host,
       dialect: 'mysql',
