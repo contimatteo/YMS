f0aa83c14ab65282cf99fbf909c82ab2527ee454
Conti Matteo <contimatteo1505@gmail.com>
2018-12-13 12:08:17 +0100

    fixed code style

--- routes/Auth.js
+++ routes/Auth.js
@@ -4,34 +4,20 @@ module.exports = function (app, passport) {
 
   app.get('/signup', AuthController.signup);
 
-
   app.get('/signin', AuthController.signin);
-
 
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/',
     failureRedirect: '/signup'
   }));
 
-
   app.get('/', AuthController.userLoggedIn, AuthController.dashboard);
-
 
   app.get('/logout', AuthController.userLoggedIn, AuthController.logout);
 
-
-  // app.post('/signin', passport.authenticate('local-signin', {
-  //   successRedirect: '/dashboard',
-  //   failureRedirect: '/signin'
-  // }));
-
-  app.post('/signin', passport.authenticate('local-signin'), function(req, res) {
-    nextUrl = req.session.previous_url || "/";
+  app.post('/signin', passport.authenticate('local-signin'), function (req, res) {
+    nextUrl = /* req.session.previous_url || */ "/";
     res.redirect(nextUrl);
-    // res.redirect("/");
   });
-
-
-
 
 }
